e377b1a4ab5b3ca0d18eadc90cd49a6a7dcd0137
strelda <strelecekja@gmail.com>
2021-07-17 16:09:21 +0200

    cleanup

---
+++ functions.js
@@ -0,0 +1,14 @@
+function goToUser(val) {
+    if (event.keyCode == 13) {
+        window.location.href="users/"+val+".html";
+    }
+};
+function goToUser1(val) {
+    if (event.keyCode == 13) {
+        window.location.href=val+".html";
+    }
+};
+const dict = {
+    "eer": "OndraTalir",
+    "eue": "OndryMamka"
+};
